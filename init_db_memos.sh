1ed59646e2da6adae0e108122f30dc3eeb9b1dea
miyanew <dev38_shin@yahoo.co.jp>
2024-05-23 03:41:00 +0900

    コードの統一感を整えた

--- init_db_memos.sh
+++ init_db_memos.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "${MEMO_DB}" ]; then
+  echo "MEMO_DB not set."
+  return 1
+fi
+
 if [ -z "${MEMO_USER}" ]; then
   echo "MEMO_USER not set."
   return 1
@@ -10,17 +15,12 @@ if [ -z "${MEMO_PASSWORD}" ]; then
   return 1
 fi
 
-if [ -z "${MEMO_DB}" ]; then
-  echo "MEMO_DB not set."
-  return 1
-fi
-
+MEMO_DB="${MEMO_DB}"
 MEMO_USER="${MEMO_USER}"
 MEMO_PASSWORD="${MEMO_PASSWORD}"
-MEMO_DB="${MEMO_DB}"
 
 sudo -u postgres psql -U postgres -c "CREATE USER $MEMO_USER WITH PASSWORD '$MEMO_PASSWORD';"
-sudo -u postgres createdb $MEMO_DB
+sudo -u postgres psql -U postgres -c "CREATE DATABASE $MEMO_DB;"
 sudo -u postgres psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE $MEMO_DB TO $MEMO_USER;"
 sudo -u postgres psql -U postgres -d $MEMO_DB -c "CREATE TABLE IF NOT EXISTS memos (id UUID PRIMARY KEY DEFAULT gen_random_uuid(), name VARCHAR NOT NULL, body VARCHAR);"
 sudo -u postgres psql -U postgres -d $MEMO_DB -c "GRANT ALL PRIVILEGES ON TABLE memos TO $MEMO_USER;"
